e37210a3e95eea6516a68f6eff9b1a758a97bc6d
Vlad Moldoveanu <vmoldoveanu@deloittece.com>
2019-08-08 11:44:42 +0300

    d3js workshop initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+/* 1. Let's create a simple data set */
+const data = [{
+        name: "a",
+        value: 100
+    },
+    {
+        name: "b",
+        value: 200
+    },
+    {
+        name: "c",
+        value: 300
+    },
+    {
+        name: "d",
+        value: 400
+    },
+    {
+        name: "e",
+        value: 500
+    },
+    {
+        name: "f",
+        value: 600
+    }
+]
